13fdf661b2f9bf716e3746adf6bd79f5a9fc2fcc
Priyanshi Bhatt <priyanshibhatt@Priyanshis-MacBook-Air.local>
2026-02-04 11:32:43 +0530

    Fix NA replacement and remove chained assignment

--- scripts/eda.py
+++ scripts/eda.py
@@ -4,26 +4,23 @@
 
 # df = pd.read_csv(CSV_PATH)
 
-# # print(df.head())
-# # print(df.shape)
-# # print(df.info())
-# # print(df.columns)
-# # print(df.isna())
-# # print(df["white_elo"].isna())
-# # print(df["site"].isin(["?"]).sum())
-# # df["date"] = df["date"].replace(
-# #     r".*\?.*",
-# #     pd.NA,
-# #     regex=True
-# # )
+
+# print(df.isna())
+# print(df["white_elo"].isna())
+# print(df["site"].isin(["?"]).sum())
+# df["date"] = df["date"].replace(
+#     r".*\?.*",
+#     pd.NA,
+#     regex=True
+# )
 
 # cols = ["event","site","date","round","white","black","white_elo","black_elo","result","result_numeric","eco","total_moves","moves_san","moves_uci","elo_diff","source_file"]
 
-# # for col in cols:
-# #     df[col] = df[col].replace(r"\?", pd.NA, regex=True, inplace=True)
+# for col in cols:
+#     df[col] = df[col].replace(r"\?", pd.NA, regex=True, inplace=True)
 
-# # print(df.isin(["?"]).sum())
-# # print(df.isna().sum())
+# print(df.isin(["?"]).sum())
+# print(df.isna().sum())
 # df[cols] = df[cols].replace(r"\?", pd.NA, regex=True)
 
 # df.to_csv("all_games.csv", index=False)
@@ -45,7 +42,11 @@ df = df.replace(r"\?", pd.NA, regex=True)
 # ---------- OPTIONAL: CHECK MISSING VALUES ----------
 print("\n🔍 Missing values after cleaning:")
 print(df.isna().sum())
-
+print(df.head())
+print(df.shape)
+print(df.info())
+print(df.columns)
+print(df.describe)
 # ---------- SAVE CLEANED DATA ----------
 df.to_csv(CSV_PATH, index=False)
 
